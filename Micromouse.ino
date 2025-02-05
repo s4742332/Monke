0b7a3d8e2c37f866a6c590d95516f7f8fd103c78
s4742332 <s4m4ts4m@gmail.com>
2025-02-05 22:20:27 +1000

    Update Micromouse.ino
    added in ultrasonic basics etc.

--- Micromouse.ino
+++ Micromouse.ino
@@ -113,24 +113,77 @@ class Maze {
       }
     }
 };
-// Define the number of steps per revolution for the 28BYJ-48 motor
-const int stepsPerRevolution = 2038;  // Full revolution for the 28BYJ-48 motor in full-step mode
 
-// Define the motor connections
-const int motorPin1 = 8;  
-const int motorPin2 = 9;  
-const int motorPin3 = 10; 
-const int motorPin4 = 11; 
+ //Set-up Motors
+#define STEPS = 2048; // Number of steps per revolution for the 28BYJ-48 motor
+  const int MaxSpeed = 255; 
+  const int Stop = 0;
 
-// Create a Stepper object
-Stepper myStepper(stepsPerRevolution, motorPin1, motorPin2, motorPin3, motorPin4);
+//Set-up Left and Right Motor Classes
+  Stepper Leftmotor(STEPS, 8, 9, 10, 11);
+  Stepper Rightmotor(STEPS, 4, 5, 6, 7);
 
+//Ultrasonic Set-up
+  const int trigPin = ;
+  const int echoPinL = ;
+  const int echoPinR = ;
+  const int echoPinF = ;
+  float duration[3], distance[3];
+  float echoPins[echoPinL, echoPinR, echoPinF];
+
+// Indicator Set-up
+  const int LightL = ;
+  const int LightR = ;
+  const int LightF = ;
+
+  int Walld = ; //distance in which a Wall is definitely in front/surrounding the mouse
+  bool WallL = ; // is wall currently there?
+  bool WallR = ; 
+  bool WallF = ; 
+  float Walls[WallL, WallR, WallF]; //
+
+// Maze Set-p
 void setup() {
   // Set the motor speed (RPM)
   myStepper.setSpeed(10);  // Set a lower speed to reduce RPM (adjust to your needs)
+
+  // Set-Up Ultrasonics
+  pinMode(trigPin, OUTPUT); //Ask for a reading
+  pinMode(echoPinL, INPUT); //Left Sensor
+  pinMode(echoPinR, INPUT); //Right Sensor
+  pinMode(echoPinF, INPUT); //Front Sensor
+  Serial.begin(9600);
+
+  //Set-Up Indicators
+  pinMode(LightL, OUTPUT); //Light Left
+  pinMode(LightR, OUTPUT); //Light Right
+  pinMode(LightF, OUTPUT); //Light Forward
 }
 
 void loop() {
-  // Rotate the motor one direction
+// Rotate the motor one direction
   myStepper.step(stepsPerRevolution);  // Rotate 1 full revolution
+
+// Sensor Reading
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH)
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = [pulsinIn(echoPinL, HIGH), pulsinIn(echoPinR, HIGH),pulsinIn(echoPinF, HIGH)];
+  distance = (duration*.0343)/2
+  
+  // Indicate if Wall
+  for (int i = 0; i =3; i++) {
+    if (distance[i] <= Walld) {
+      analogWrite(echoPins[i], HIGH);
+      Walls[i] = True;
+    }
+    else {
+      analogWrite(echoPins[i], LOW);
+      Walls[i] = False;
+    }
+  } 
+
 }
